5a96a474244f63e6202fbb50a0ed53c85ad04b4d
AaronSledge <ahern2118@gmail.com>
2025-11-08 19:30:11 -0800

    Output almost done wip

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import threading
 import time
 import os
 from kMeansAlg import KM
+from print import output
 
 # This function reads in the input file which has coordinates for each location
 def FileRead(filename):
@@ -84,13 +85,58 @@ def finalPathToFile(filename, finalPath, collectionOfDistance):
 filename = input("Enter the name of file: ")
 listOfPoints = FileRead(filename)
 dictionary = KM(listOfPoints)
-c1 = dictionary['dict4']['center1']
-cr1 = dictionary['dict4']['cluster1']
 
-for i in range(len(cr1)):
-    print (cr1[i].newNumber)
+landing1_center1 = dictionary['dict1']['center1']
+landing1_cluster1 = dictionary['dict1']['cluster1']
 
-dm = dist_matrix(cr1)
+landing2_center1 = dictionary['dict2']['center1']
+landing2_cluster1 = dictionary['dict2']['cluster1']
+
+landing2_center2 = dictionary['dict2']['center2']
+landing2_cluster2 = dictionary['dict2']['cluster2']
+
+landing3_center1 = dictionary['dict3']['center1']
+landing3_cluster1 = dictionary['dict3']['cluster1']
+
+landing3_center2 = dictionary['dict3']['center2']
+landing3_cluster2 = dictionary['dict3']['cluster2']
+
+landing3_center3 = dictionary['dict3']['center3']
+landing3_cluster3 = dictionary['dict3']['cluster3']
+
+landing4_center1 = dictionary['dict4']['center1']
+landing4_cluster1 = dictionary['dict4']['cluster1']
+
+landing4_center2 = dictionary['dict4']['center2']
+landing4_cluster2 = dictionary['dict4']['cluster2']
+
+landing4_center3 = dictionary['dict4']['center3']
+landing4_cluster3 = dictionary['dict4']['cluster3']
+
+landing4_center4 = dictionary['dict4']['center4']
+landing4_cluster4 = dictionary['dict4']['cluster4']
+
+
+#testdistance1, testpath1, _, _ = ClassicNN(landing2_cluster1, dist_matrix(landing2_cluster1))
+landing1 = []
+landing2 = []
+landing3 = []
+landing4 = []
+
+landing1.append((landing1_cluster1, dist_matrix(landing1_cluster1), landing1_center1))
+
+landing2.append((landing2_cluster1, dist_matrix(landing2_cluster1), landing2_center1))
+landing2.append((landing2_cluster2, dist_matrix(landing2_cluster2), landing2_center2))
+
+landing3.append((landing3_cluster1, dist_matrix(landing3_cluster1), landing3_center1))
+landing3.append((landing3_cluster2, dist_matrix(landing3_cluster2), landing3_center2))
+landing3.append((landing3_cluster3, dist_matrix(landing3_cluster3), landing3_center3))
+
+landing4.append((landing4_cluster1, dist_matrix(landing4_cluster1), landing4_center1))
+landing4.append((landing4_cluster2, dist_matrix(landing4_cluster2), landing4_center2))
+landing4.append((landing4_cluster3, dist_matrix(landing4_cluster3), landing4_center3))
+landing4.append((landing4_cluster4, dist_matrix(landing4_cluster4), landing4_center4))
+
 # Random NN Functions
 
 NNisDone = False
@@ -101,40 +147,57 @@ prev = 0
 
 # This function prints the total distance of a path calculated using classic nn in the first iteration 
 # It then uses a loop which calls our modified nn algorithm to try and find a shorter path
-def printSumNN(sumOfDistance, listOfPoints, dist_mat):
-    global collectionOfDistanceNN, finalPathNN, prev, first_iter 
-    start_time = time.time()
-    # If it is the first iteration classic nn should be performed
-    if first_iter == True:
-        time.sleep(0.25)
-        sumOfDistance, path, _, _ = ClassicNN(listOfPoints, dist_mat)
-        # Just like random search if the new distance is not the same as the old distance we found a new route
-        if prev != sumOfDistance:
-            # This gets the time it took to find a route
-            time_So_Far = time.time() - start_time
-            # For jason when making distance over time graph
-            collectionOfDistanceNN.append((sumOfDistance, time_So_Far)) 
-             # For jason when making route graph and kenny for file output
-            finalPathNN = path
-            print(f"          {sumOfDistance}")
-        prev = sumOfDistance
-        # Make sure to indicate this after one run so classic nn only runs once
-        first_iter = False
-    # While the user has not hit the entry key this loop will keep going 
-    while not NNisDone:
-        #code pauses a quarter of a second. Can change if needed to
-        time.sleep(0.25) 
-        sumOfDistance, path, _, _ = ModifiedNN(listOfPoints, dist_mat, finalPathNN, dist_to_beat=sumOfDistance)
-        # This is utilized the same
-        if prev != sumOfDistance:
-            time_So_Far = time.time() - start_time
-            collectionOfDistanceNN.append((sumOfDistance, time_So_Far)) #for jason when making distance over time graph
-            finalPathNN = path #for jason when making route graph
-            print(f"          {sumOfDistance}")
-        prev = sumOfDistance  
-       
-    saveRouteImg(listOfPoints, finalPathNN, prev, filename)
+def printSumNN(listOfPoints, landing1, landing2, landing3, landing4):
+    landing1_distances = []
+    landing2_distances = []
+    landing3_distances = []
+    landing4_distances = []
+    print(f"There are {len(listOfPoints)} nodes:")
+    for i, j, k in landing1:
+        finalDistance, time_so_far = output(i, j)
+        landing1_distances.append((finalDistance, time_so_far))
+    
+    print(f"1) If you use {len(landing1)} drone (s), the total route will be {sum(landing1_distances[0])} meters")
+    print(f"    i. Landing pad 1 should be at [{int(landing1[0][2].x)},{int(landing1[0][2].y)}], serving {len(landing1[0][0])} locations, route is {landing1_distances[0][0]} meters")
 
+    for i, j, k in landing2:
+        finalDistance, time_so_far = output(i, j)
+        landing2_distances.append((finalDistance, time_so_far))
+    
+    print(f"2) If you use {len(landing2)} drone (s), the total route will be {sum(landing2_distances[0])} meters")
+    print(f"    i. Landing pad 1 should be at [{int(landing2[0][2].x)},{int(landing2[0][2].y)}], serving {len(landing2[0][0])} locations, route is {landing2_distances[0][0]} meters")
+    print(f"    ii. Landing pad 2 should be at [{int(landing2[1][2].x)},{int(landing2[1][2].y)}], serving {len(landing2[1][0])} locations, route is {landing2_distances[1][0]} meters")
+
+    for i, j, k in landing3:
+        finalDistance, time_so_far = output(i, j)
+        landing3_distances.append((finalDistance, time_so_far))
+    
+    print(f"3) If you use {len(landing3)} drone (s), the total route will be {sum(landing3_distances[0])} meters")
+    print(f"    i. Landing pad 1 should be at [{int(landing3[0][2].x)},{int(landing3[0][2].y)}], serving {len(landing3[0][0])} locations, route is {landing3_distances[0][0]} meters")
+    print(f"    ii. Landing pad 2 should be at [{int(landing3[1][2].x)},{int(landing3[1][2].y)}], serving {len(landing3[1][0])} locations, route is {landing3_distances[1][0]} meters")
+    print(f"    iii. Landing pad 3 should be at [{int(landing3[2][2].x)},{int(landing3[2][2].y)}], serving {len(landing3[2][0])} locations, route is {landing3_distances[2][0]} meters")
+
+
+    for i, j, k in landing4:
+        finalDistance, time_so_far = output(i, j)
+        landing4_distances.append((finalDistance, time_so_far))
+    
+    print(f"4) If you use {len(landing4)} drone (s), the total route will be {sum(landing4_distances[0])} meters")
+    print(f"    i. Landing pad 1 should be at [{int(landing4[0][2].x)},{int(landing4[0][2].y)}], serving {len(landing4[0][0])} locations, route is {landing4_distances[0][0]} meters")
+    print(f"    ii. Landing pad 2 should be at [{int(landing4[1][2].x)},{int(landing4[1][2].y)}], serving {len(landing4[1][0])} locations, route is {landing4_distances[1][0]} meters")
+    print(f"    iii. Landing pad 3 should be at [{int(landing4[2][2].x)},{int(landing4[2][2].y)}], serving {len(landing4[2][0])} locations, route is {landing4_distances[2][0]} meters")
+    print(f"    iv. Landing pad 4 should be at [{int(landing4[3][2].x)},{int(landing4[3][2].y)}], serving {len(landing4[3][0])} locations, route is {landing4_distances[3][0]} meters")
+    
+    bestSolution = int(input("Please select your choice 1 to 4: "))
+    if bestSolution == 1:
+        return landing1, landing1_distances
+    if bestSolution == 2:
+        return landing2, landing2_distances
+    if bestSolution == 3:
+        return landing3, landing3_distances
+    if bestSolution == 4:
+        return landing4, landing4_distances
+    
 
 def writeToDistanceFileNN(collectionOfDistanceNN):
     with open("distanceFileRandomNN.txt", "a") as file:
@@ -145,26 +208,27 @@ def writeToDistanceFileNN(collectionOfDistanceNN):
     #print(listOfPoints[i].number)
 
 
-print(f"There are {(len(listOfPoints))} nodes, computing route..")
-print("     Shortest Route Discovered So Far")
-
 # Used threading so function can continously run without having to wait for input
-threading.Thread(target=printSumNN, args=(math.inf, cr1, dm)).start() 
+#threading.Thread(target=printSumNN, args=(math.inf, landing1, landing2, landing3, landing4)).start() 
+
+best_cluster, best_distance = printSumNN(listOfPoints, landing1, landing2, landing3, landing4)
+
 
 # While the function input is awaiting input from user print sum runs
-input()
+#input()
 # After input the loop condition is set to true so it stops
-NNisDone = True
+#NNisDone = True
 
-if (collectionOfDistanceNN[-1][0] > 10000):
-        print(f"Warning: Solution is {collectionOfDistanceNN[-1][0]}, greater than the 6000-meter constraint.")
+#if (collectionOfDistanceNN[-1][0] > 10000):
+        #print(f"Warning: Solution is {collectionOfDistanceNN[-1][0]}, greater than the 6000-meter constraint.")
 
 filename = os.path.splitext(os.path.basename(filename))[0]
 # this writes the solution for which nodes to visit e.g. "1 2 10 3 1"
 
-outFile = finalPathToFile(filename, finalPathNN, collectionOfDistanceNN)
+#outFile = finalPathToFile(filename, finalPathNN, collectionOfDistanceNN)
 
-print(f"Route written to disk as {outFile}")
+for i in range(len(best_cluster)):
+    print(f"Writting {filename}_{i + 1}_SOLUTION_{best_distance[i][0]}.txt")
 
 writeToDistanceFileNN(collectionOfDistanceNN)
 
